22499c0f526fe547b368faf7c7510facb6299cfa
NoeStorg4to <noelia.storgato@gmail.com>
2025-10-08 10:17:25 -0300

    Optimizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,25 +15,41 @@ import { Ranking } from './components/ranking/ranking';
 
 
 
+// export const routes: Routes = [
+//     { path: '', redirectTo: '/home', pathMatch: 'full' },
+//     { path: 'login', component: Login, canActivate:[authGuard] }, 
+//     { path: 'registro', component: Registro, canActivate:[authGuard] },
+//     { path: 'home', component: Home },
+//     { path: 'quien-soy', component: QuienSoy },
+
+//     { path: 'ahorcado', component: AhorcadoGame, canActivate: [ageRestrictionGuard] },
+//     { path: 'mayorMenor', component: MayorMenorGame },
+//     { path: 'preguntados', component: Preguntados, canActivate:[authGuard] },
+//     { path: 'adivina-numero', component: AdivinaNumero, canActivate:[authGuard] },
+
+//     { path: 'ranking', component: Ranking},
+    
+//     { path: 'chat', component: SalaChat },
+
+//     { path: '**', redirectTo: '/home' }
+// ];
+
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'login', component: Login, canActivate:[authGuard] }, 
-    { path: 'registro', component: Registro, canActivate:[authGuard] },
-    { path: 'home', component: Home },
-    { path: 'quien-soy', component: QuienSoy },
-
-    { path: 'ahorcado', component: AhorcadoGame, canActivate: [ageRestrictionGuard] },
-    { path: 'mayorMenor', component: MayorMenorGame },
-    { path: 'preguntados', component: Preguntados, canActivate:[authGuard] },
-    { path: 'adivina-numero', component: AdivinaNumero },
-
-    { path: 'ranking', component: Ranking},
     
-    { path: 'chat', component: SalaChat },
-
+    { path: 'login', loadComponent: () => import('./components/login/login').then(m => m.Login), canActivate: [authGuard] },
+    { path: 'registro', loadComponent: () => import('./components/registro/registro').then(m => m.Registro), canActivate: [authGuard] },
+    { path: 'home', loadComponent: () => import('./components/home/home').then(m => m.Home) },
+    { path: 'quien-soy', loadComponent: () => import('./components/quien-soy/quien-soy').then(m => m.QuienSoy) },
+    { path: 'ahorcado', loadComponent: () => import('./components/ahorcado-game/ahorcado-game').then(m => m.AhorcadoGame), canActivate: [ageRestrictionGuard] },
+    { path: 'mayorMenor', loadComponent: () => import('./components/mayor-menor.game/mayor-menor.game').then(m => m.MayorMenorGame) },
+    { path: 'preguntados', loadComponent: () => import('./components/preguntados/preguntados').then(m => m.Preguntados), canActivate: [authGuard] },
+    { path: 'adivina-numero', loadComponent: () => import('./components/adivina-numero/adivina-numero').then(m => m.AdivinaNumero), canActivate: [authGuard] },
+    { path: 'ranking', loadComponent: () => import('./components/ranking/ranking').then(m => m.Ranking) },
+    { path: 'chat', loadComponent: () => import('./components/sala.chat/sala.chat').then(m => m.SalaChat) },
+    
     { path: '**', redirectTo: '/home' }
 ];
-
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
